b7934911c7c21bfa2f3594814cc301a2de8530b2
James F. Thomas <james.fthomas@yahoo.com>
2020-08-31 12:59:28 -0500

    anges to package/plock/&& server/serv.js files

--- server/server.js
+++ server/server.js
@@ -1,31 +1,30 @@
 // import express module into file
 const express = require('express');
 
-//create variable set to new express instance
+// create variable set to new express instance
 const app = express();
 
-//set port for server to run on with backup port
+// set port for server to run on with backup port
 const port = process.env.PORT || 3000;
 
 // creat dummy data to test routes
-const groupName = "Don't Phase Me Bros."
-const projectName = "Game Time"
+const groupName = "Don't Phase Me Bros.";
+const projectName = 'Game Time';
 const dummyData = {
   group: `${groupName}`,
-  project: `${projectName}`
-}
+  project: `${projectName}`,
+};
 
 // Create Test Routes
-app.get('/api', (req, res)=>{
+app.get('/api', (req, res) => {
   res.send(dummyData);
-})
+});
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
   res.send('Hello World');
-})
+});
 
 // set server to listen for events on PORT
 app.listen(port, () => {
-  console.log(`This server only listens to:${port}`)
+  console.log(`This server only listens to:${port}`);
 });
-
