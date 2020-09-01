588a92cda47c05edf5fb55dc3fcf8c5f780efb63
gduple <gduple@gmail.com>
2020-09-01 00:27:28 -0500

    getUser, addUser implemented

--- database/index.js
+++ database/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 const { Pool } = require('pg');
 // Client?
 
@@ -8,14 +9,74 @@ const pool = new Pool({
   password: '',
 });
 
-const getUser = (id) => pool
-  .query('SELECT * FROM users WHERE id = ?', [id])
-  .then((res) => res.rows)
-  // .then(() => {
-  //   pool.query('SELECT * FROM scores WHERE id_user = user.id')
-  // })
-  .catch((err) => console.error(err));
+const getUserCommand = `
+  SELECT users.*
+  FROM users
+  WHERE id = $1
+`;
+
+const getUserScoreCommand = `
+  SELECT scores.*
+  FROM scores
+  WHERE id_user = $1
+`;
+
+const getUser = (id) => {
+  let user;
+  return pool.query(getUserCommand, [id])
+    .then((res) => {
+      [user] = res.rows;
+      return pool.query(getUserScoreCommand, [user.id]);
+    })
+    .then((scores) => {
+      user.scores = scores.rows;
+      return user;
+    })
+    .catch((err) => { throw err; });
+};
+
+// async function getUser(id) {
+//   console.log('ID: ', id);
+//   let user = await pool.query(getUserCommand, [id]);
+//   console.log('USER: ', user.rows);
+//   const scores = await pool.query(getUserScoreCommand, [id]);
+//   console.log('SCORES: ', scores.rows);
+//   user = user.rows;
+//   user.scores = scores.rows;
+//   return user;
+// }
+
+const addUserCommand = `
+  INSERT INTO users (id_discord, username, profile_photo_url, location, age)
+  VALUES ($1, $2, $3, $4, $5);
+`;
+
+const getAddededUserCommand = `
+  SELECT *
+  FROM users
+  WHERE id_discord = $1
+`;
+
+async function addUser(userObj) {
+  const {
+    id_discord, username, profile_photo_url, location, age,
+  } = userObj;
+  await pool.query(addUserCommand, [id_discord, username, profile_photo_url, location, age]);
+  let addedUser = await pool.query(getAddededUserCommand, [id_discord]);
+  addedUser = addedUser.rows;
+  addedUser[0].scores = [];
+  return addedUser;
+}
+
+// const postUser = (userObj) => {
+//   const {
+//     id_discord, username, profile_photo_url, location, age,
+//   } = userObj;
+//   return pool.query(postUserCommand, [id_discord, username, profile_photo_url, location, age])
+//     .catch((err) => { throw err; });
+// };
 
 module.exports = {
   getUser,
+  addUser,
 };
