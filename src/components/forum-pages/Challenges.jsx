0a6389b40ffd00c7444c81bfccac5fbb4c1bea67
benyoss <benyoss4@gmail.com>
2020-09-01 20:09:06 -0500

    "Added basic component skeleton for challenges."

--- src/components/forum-pages/Challenges.jsx
+++ src/components/forum-pages/Challenges.jsx
@@ -2,3 +2,18 @@ import React from 'react';
 import {
   Nav, Navbar, Container,
 } from 'react-bootstrap';
+
+const Challenges = () => (
+  <div>
+    <div className="Challenges-Header">
+      <h2>
+        This is the challenges header
+      </h2>
+    </div>
+    <div className="Challenges-Body">
+      <h1>This is the challenges body</h1>
+    </div>
+  </div>
+);
+
+export default Challenges;
