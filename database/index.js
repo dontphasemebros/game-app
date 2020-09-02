d0bfdf26401445808e8c65cc9099708a4deb380e
gduple <gduple@gmail.com>
2020-09-01 19:44:03 -0500

    fixed error handling

--- database/index.js
+++ database/index.js
@@ -8,55 +8,31 @@ const pool = new Pool({
   password: process.env.DB_PASS,
 });
 
-const getUserCommand = `
-  SELECT users.*
-  FROM users
-  WHERE id = $1
-`;
-
-const getUserScoreCommand = `
-  SELECT scores.*
-  FROM scores
-  WHERE id_user = $1
-`;
-
 // takes a number representing the user's database ID (NOT the Discord ID)
 // returns array containing user object with a nested array of the user's scores
-const getUser = (id) => {
-  let user;
-  return pool.query(getUserCommand, [id])
-    .then((res) => {
-      [user] = res.rows;
-      return pool.query(getUserScoreCommand, [user.id]);
-    })
-    .then((scores) => {
-      user.scores = scores.rows;
-      return user;
-    })
-    .catch((err) => err);
-};
+async function getUser(id) {
+  const getUserCommand = `
+    SELECT users.*
+    FROM users
+    WHERE id = $1
+  `;
 
-// async function getUser(id) {
-//   console.log('ID: ', id);
-//   let user = await pool.query(getUserCommand, [id]);
-//   console.log('USER: ', user.rows);
-//   const scores = await pool.query(getUserScoreCommand, [id]);
-//   console.log('SCORES: ', scores.rows);
-//   user = user.rows;
-//   user.scores = scores.rows;
-//   return user;
-// }
+  const getUserScoreCommand = `
+    SELECT scores.*
+    FROM scores
+    WHERE id_user = $1
+  `;
 
-const addUserCommand = `
-  INSERT INTO users (id_discord, username, profile_photo_url, location, age)
-  VALUES ($1, $2, $3, $4, $5);
-`;
-
-const getAddededUserCommand = `
-  SELECT *
-  FROM users
-  WHERE id_discord = $1
-`;
+  try {
+    let user = await pool.query(getUserCommand, [id]);
+    const scores = await pool.query(getUserScoreCommand, [id]);
+    user = user.rows;
+    if (scores) user[0].scores = scores.rows;
+    return user;
+  } catch (error) {
+    return console.error('COULD NOT GET USER FROM DATABASE', error);
+  }
+}
 
 // takes an object with user properties: idDiscord, username, profilePhotoUrl, location, age
 // returns array containing newly created user object
@@ -65,20 +41,27 @@ async function addUser(userObj) {
     idDiscord, username, profilePhotoUrl, location, age,
   } = userObj;
 
-  await pool.query(addUserCommand, [idDiscord, username, profilePhotoUrl, location, age]);
-  let addedUser = await pool.query(getAddededUserCommand, [idDiscord]);
-  addedUser = addedUser.rows;
-  addedUser[0].scores = [];
-  return addedUser;
-}
+  const addUserCommand = `
+    INSERT INTO users (id_discord, username, profile_photo_url, location, age)
+    VALUES ($1, $2, $3, $4, $5);
+  `;
 
-// const postUser = (userObj) => {
-//   const {
-//     id_discord, username, profile_photo_url, location, age,
-//   } = userObj;
-//   return pool.query(postUserCommand, [id_discord, username, profile_photo_url, location, age])
-//     .catch((err) => { throw err; });
-// };
+  const getAddededUserCommand = `
+    SELECT *
+    FROM users
+    WHERE id_discord = $1
+  `;
+
+  try {
+    await pool.query(addUserCommand, [idDiscord, username, profilePhotoUrl, location, age]);
+    let addedUser = await pool.query(getAddededUserCommand, [idDiscord]);
+    addedUser = addedUser.rows;
+    addedUser[0].scores = [];
+    return addedUser;
+  } catch (error) {
+    return console.error('COULD NOT ADD USER TO DATABASE', error);
+  }
+}
 
 module.exports = {
   getUser,
