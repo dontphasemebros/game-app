a5bc72ca897c1bf6f64cb7513beac002e5f511cd
benyoss <benyoss4@gmail.com>
2020-09-01 19:06:43 -0500

    "Created new directory to house all components/files that pertain to the forum-side."

---
+++ src/components/forum-pages/DiscussionPage.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import {
+  Nav, Navbar, Container,
+} from 'react-bootstrap';
---
+++ src/components/forum-pages/GamerNews.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  Nav, Navbar, Container,
+} from 'react-bootstrap';
+
+const GamerNews = () => (
+  <div>
+    <div className="Gamer-News-Header">
+      <h2>
+        This is the gamer news header
+      </h2>
+    </div>
+    <div className="Gamer-News-Body">
+      <h1>This is the gamer news body</h1>
+    </div>
+  </div>
+);
+
+export default GamerNews;
