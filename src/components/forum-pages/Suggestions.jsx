73ad692f7fbf02010daaad7c8f595cd7cd490451
benyoss <benyoss4@gmail.com>
2020-09-01 20:11:19 -0500

    "Added a basic skeleton for the suggestions component "

--- src/components/forum-pages/Suggestions.jsx
+++ src/components/forum-pages/Suggestions.jsx
@@ -2,3 +2,18 @@ import React from 'react';
 import {
   Nav, Navbar, Container,
 } from 'react-bootstrap';
+
+const Suggestions = () => (
+  <div>
+    <div className="Suggestions-Header">
+      <h2>
+        This is the Suggestions header
+      </h2>
+    </div>
+    <div className="Suggestions-Body">
+      <h1>This is the Suggestions body</h1>
+    </div>
+  </div>
+);
+
+export default Suggestions;
