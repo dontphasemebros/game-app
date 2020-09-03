c555810d51879233ffbaa9e86085d2510d5735a1
benyoss <benyoss4@gmail.com>
2020-09-03 03:32:20 -0500

    "Finished with basic forum page layout and discussion styles."

--- src/components/Forum.jsx
+++ src/components/Forum.jsx
@@ -2,61 +2,101 @@ import React from 'react';
 import {
   Navbar,
 } from 'react-bootstrap';
+import {
+  BrowserRouter, Switch, Route,
+} from 'react-router-dom';
+import GeneralDiscussion from './forum-pages/DiscussionPage';
+import Challenges from './forum-pages/Challenges';
+import Suggestions from './forum-pages/Suggestions';
+import GamerNews from './forum-pages/GamerNews';
+
+//--------------------------------------------------------------------------------
+// LIST OF METHODS TO USE FOR DISCUSSIONS
+
+// RENDERS N NUMBER OF DISCUSSIONS UNDER A CHANNEL
+
+const forEachDiscussion = () => {
+  const storage = [];
+  for (let i = 0; i < 1; i += 1) {
+    storage.push(
+      <div className="panel-primary inline-block" id="GeneralDisussion" style={{ backgroundColor: '#D6DBDF', minWidth: '1100px' }}>
+        <div className={`profile-picture${i} panel-body text-left inline-block`}>
+          <div className="bg-secondary" style={{ display: 'inline-block', minWidth: '300px' }}>
+            <img className="d-print-inline-block" src="https://www.nationalgeographic.com/content/dam/animals/2019/10/goldfish/01-goldfish-nationalgeographic_1567132.jpg" height="100px" width="100px" alt="" style={{ display: 'inline-block' }} />
+            <div className={`username${i} panel-body text-left inline-block`} style={{ display: 'inline-block' }}>
+              <h5>Goldfish</h5>
+            </div>
+            <div className={`date${i} panel-body text-left inline-block`} style={{ display: 'inline-block' }}>
+              <span style={{ marginRight: '20px' }}>10/20/2020</span>
+            </div>
+          </div>
+          <h4 style={{ display: 'inline-block' }}>Listeninginginging to the greatest tune</h4>
+        </div>
+      </div>,
+    );
+  }
+  return storage;
+};
+//--------------------------------------------------------------------------------
 
 const Forum = () => (
-  <div>
-    <div className="ForumHeader">
-      <h1>This is the forum header:</h1>
+  <BrowserRouter>
+    <div>
+      <Switch>
+        <Route path="/forum">
+          <div style={{ padding: '20px' }}>
+            <div className="card text-white bg-secondary mb-3">
+              <h2 className="card-header">
+                Game-Time Forum
+              </h2>
+            </div>
+          </div>
+          <div className="ForumBody list-group">
+            <div style={{ padding: '20px' }}>
+              <li className="Forum-Topic-General-Discussion list-group-item" style={{ backgroundColor: '#ececec' }}>
+                <Navbar.Brand href="/discussion">
+                  <h2>This is the topic for General-Discussion</h2>
+                </Navbar.Brand>
+              </li>
+            </div>
+            <div style={{ padding: '20px' }}>
+              <li className="Forum-Topic-Challenges list-group-item" style={{ backgroundColor: '#ececec' }}>
+                <Navbar.Brand href="/challenges">
+                  <h2>This is the topic for Challenges</h2>
+                </Navbar.Brand>
+              </li>
+            </div>
+            <div style={{ padding: '20px' }}>
+              <li className="Forum-Topic-Suggestions list-group-item" style={{ backgroundColor: '#ececec' }}>
+                <Navbar.Brand href="/suggestions">
+                  <h2>This is the topic for Suggestions</h2>
+                </Navbar.Brand>
+              </li>
+            </div>
+            <div style={{ padding: '20px' }}>
+              <li className="Forum-Topic-Gamer-News list-group-item" style={{ backgroundColor: '#ececec' }}>
+                <Navbar.Brand href="/gamer-news">
+                  <h2>This is the topic for Gamer News</h2>
+                </Navbar.Brand>
+              </li>
+            </div>
+          </div>
+        </Route>
+        <Route exact path="/discussion">
+          <GeneralDiscussion forEachDiscussion={forEachDiscussion} />
+        </Route>
+        <Route exact path="/challenges">
+          <Challenges forEachChallenge={forEachDiscussion} />
+        </Route>
+        <Route exact path="/suggestions">
+          <Suggestions forEachSuggestion={forEachDiscussion} />
+        </Route>
+        <Route exact path="/gamer-news">
+          <GamerNews />
+        </Route>
+      </Switch>
     </div>
-    <div className="ForumBody">
-      <h1>This is the forum Body</h1>
-      <div className="Forum-Topic-General-Discussion" style={{ backgroundColor: '#ececec', minHeight: '60px' }}>
-<<<<<<< HEAD
-        <Navbar.Brand href="/discussion">
-        <h2 style={{ positon: 'absolute', marginTop: '20px' }}>This is the topic for General-Discussion</h2>
-      </Navbar.Brand>
-      </div>
-      <div className="Forum-Topic-Challenges" style={{ backgroundColor: '#ececec', minHeight: '60px' }}>
-        <Navbar.Brand href="/challenges">
-        <h2>This is the topic for Challenges</h2>
-        </Navbar.Brand>
-      </div>
-      <div className="Forum-Topic-Suggestions" style={{ backgroundColor: '#ececec', minHeight: '60px' }}>
-        <Navbar.Brand href="/suggestions">
-        <h2>This is the topic for Suggestions</h2>
-        </Navbar.Brand>
-      </div>
-      <div className="Forum-Topic-Gamer-News" style={{ backgroundColor: '#ececec', minHeight: '60px' }}>
-        <Navbar.Brand href="/gamer-news">
-        <h2>This is the topic for Gamer News</h2>
-=======
-        <button type="submit" style={{ position: 'absolute', marginLeft: '750px', marginTop: '20px' }}>Click to start a discussion</button>
-        <Navbar.Brand href="/topic:discussion">
-          <h2 style={{ positon: 'absolute', marginTop: '20px' }}>This is the topic for General-Discussion</h2>
-        </Navbar.Brand>
-      </div>
-      <div className="Forum-Topic-Challenges" style={{ backgroundColor: '#ececec', minHeight: '60px' }}>
-        <button type="submit" style={{ position: 'absolute', marginLeft: '750px', marginTop: '20px' }}>Click to start a discussion</button>
-        <Navbar.Brand href="/topic:challenges">
-          <h2>This is the topic for Challenges</h2>
-        </Navbar.Brand>
-      </div>
-      <div className="Forum-Topic-Suggestions" style={{ backgroundColor: '#ececec', minHeight: '60px' }}>
-        <button type="submit" style={{ position: 'absolute', marginLeft: '750px', marginTop: '20px' }}>Click to start a discussion</button>
-        <Navbar.Brand href="/topic:suggestions">
-          <h2>This is the topic for Suggestions</h2>
-        </Navbar.Brand>
-      </div>
-      <div className="Forum-Topic-Gamer-News" style={{ backgroundColor: '#ececec', minHeight: '60px' }}>
-        <button type="submit" style={{ position: 'absolute', marginLeft: '750px', marginTop: '20px' }}>Click to start a discussion</button>
-        <Navbar.Brand href="/topic:gamer-news">
-          <h2>This is the topic for Gamer News</h2>
->>>>>>> 67322e67a23567c7a4297cf425a452322b10c3a0
-        </Navbar.Brand>
-      </div>
-    </div>
-    <br />
-  </div>
+  </BrowserRouter>
 );
 
 export default Forum;
