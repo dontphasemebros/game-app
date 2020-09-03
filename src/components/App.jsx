d4965b647c629e241992fba919eed105d177ff22
benyoss <benyoss4@gmail.com>
2020-09-03 03:31:47 -0500

    "Removed specific routes tied to forum discussions and moved them into forum component."

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,15 +7,7 @@ import SplashPage from './SplashPage';
 import GameTimeLogo from './GameTimeLogo';
 import GamePage from './Game';
 import Forum from './Forum';
-<<<<<<< HEAD
-import GeneralDiscussion from './forum-pages/DiscussionPage';
-import Challenges from './forum-pages/Challenges';
-import Suggestions from './forum-pages/Suggestions';
-import GamerNews from './forum-pages/GamerNews';
-import Replies from './forum-pages/ForumReplies';
-=======
 import Chat from './Chat';
->>>>>>> 67322e67a23567c7a4297cf425a452322b10c3a0
 
 function App() {
   return (
@@ -39,24 +31,25 @@ function App() {
               <NavBar />
               <Forum />
             </Route>
+            <Route path="/discussion">
+              <NavBar />
+              <Forum />
+            </Route>
+            <Route path="/suggestions">
+              <NavBar />
+              <Forum />
+            </Route>
+            <Route path="/challenges">
+              <NavBar />
+              <Forum />
+            </Route>
+            <Route path="/gamer-news">
+              <NavBar />
+              <Forum />
+            </Route>
             <Route path="/profile">
               <NavBar />
               PROFILE
-            </Route>
-            <Route path="/discussion">
-              <GeneralDiscussion />
-            </Route>
-            <Route path="/challenges">
-              <Challenges />
-            </Route>
-            <Route path="/suggestions*">
-              <Suggestions />
-            </Route>
-            <Route path="/gamer-news">
-              <GamerNews />
-            </Route>
-            <Route path="/replies">
-              <Replies />
             </Route>
             <Route path="/404">
               <NavBar />
