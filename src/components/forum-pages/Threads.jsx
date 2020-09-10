88e0574974ce68fc80775233ad77dba803686820
gduple <gduple@gmail.com>
2020-09-10 17:14:26 -0500

    (add) regexp to thread and reply to prevent blank submissions

--- src/components/forum-pages/Threads.jsx
+++ src/components/forum-pages/Threads.jsx
@@ -16,9 +16,11 @@ const Threads = ({ channel, user }) => {
   const [reload, setReload] = useState([]);
 
   const onSubmit = ({ textarea }) => {
-    if (textarea !== '') {
+    console.log('TEXTAREA: ', [textarea.trim()]);
+    if (textarea.split(/[\s\n\r\t]/).filter((str) => str.length).length) {
+      console.log('TEXTAREA TEST: ', textarea.split(/\n /).filter((chars) => chars.length).length);
       const threadObj = {
-        text: textarea,
+        text: textarea.trim(),
         idUser: user.idUser,
         idChannel,
       };
