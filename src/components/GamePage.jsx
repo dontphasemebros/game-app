84315acb1a445aba7f276d7166ed851c0f8fe2db
James F. Thomas <james.fthomas@yahoo.com>
2020-09-15 21:17:41 -0500

    (refactored) Games pages shows games in carousel

--- src/components/GamePage.jsx
+++ src/components/GamePage.jsx
@@ -1,107 +1,83 @@
-import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
-import { ToastContainer, toast } from 'react-toastify';
-import GTL from '../assets/favicon.png';
-import asteroid from '../assets/asteroids.gif';
-import starhunter from '../assets/starhunter.gif';
-import breakout from '../assets/BREAKOUT.gif';
-import slime from '../assets/slimes.gif';
-import starsollector from '../assets/starcollecter.gif';
+import React from 'react';
+import {
+  MDBCarousel, MDBCarouselCaption, MDBCarouselInner, MDBCarouselItem, MDBView, MDBContainer,
+} from 'mdbreact';
+import ben from '../assets/ben.jpg';
+import connor from '../assets/cschratz.jpg';
+import james from '../assets/slim.jpg';
+import gmoney from '../assets/gmoney.png';
+import close from '../assets/closeIcon.png';
 
-const GamePage = () => {
-  const [room, setRoom] = useState('');
-  const styles = {
-    marginBottom: '3px',
-    marginRight: '5px',
-  };
+const GamePage = () => (
+  <MDBContainer>
+    <MDBCarousel
+      activeItem={1}
+      length={5}
+      // eslint-disable-next-line react/jsx-boolean-value
+      showControls={true}
+      // eslint-disable-next-line react/jsx-boolean-value
+      showIndicators={true}
+      className="z-depth-1"
+    >
+      <MDBCarouselInner>
 
-  const gifStyle = {
-    height: 250,
-    width: 350,
-    borderRadius: '5%',
-  };
+        <MDBCarouselItem itemId="1">
+          <MDBView>
+            <img src={ben} alt="First slide" className=" w-100" />
+          </MDBView>
+          <MDBCarouselCaption>
+            <h5 href="/gameone">
+              Space Blaster
+            </h5>
+          </MDBCarouselCaption>
+        </MDBCarouselItem>
 
-  const div = {
-    textAlign: 'center',
-  };
+        <MDBCarouselItem itemId="2">
+          <MDBView>
+            <img src={james} alt="Second slide" className="d-block w-100" />
+          </MDBView>
+          <MDBCarouselCaption>
+            <h3 href="/gametwo">
+              Star Hunter
+            </h3>
+          </MDBCarouselCaption>
+        </MDBCarouselItem>
 
-  const roomToast = () => toast('Room Selected! Start Your Game!');
+        <MDBCarouselItem itemId="3">
+          <MDBView>
+            <img src={connor} alt="Third slide" className="d-block w-100" />
+          </MDBView>
+          <MDBCarouselCaption>
+            <h5 href="/gamethree">
+              Pickup Stars
+            </h5>
+          </MDBCarouselCaption>
+        </MDBCarouselItem>
 
-  const handleRoom = (e) => {
-    setRoom(e.target.value);
-  };
+        <MDBCarouselItem itemId="4">
+          <MDBView>
+            <img src={gmoney} alt="Fourth slide" className="d-block w-100" />
+          </MDBView>
+          <MDBCarouselCaption>
+            <h5 href="/breakout">
+              Break Out
+            </h5>
+          </MDBCarouselCaption>
+        </MDBCarouselItem>
 
-  return (
-    <div style={div}>
-      <br />
-      <img src={asteroid} alt="asteroid-gameplay" style={gifStyle} />
-      <h5>
-        Destroy asteroids and aliens to increase your score!
-      </h5>
-      <div className="d-flex justify-content-center">
-        <a className="btn btn-outline-dark" href="/gameone" role="button">
-          <img width="20px" style={styles} alt="asteroids" src={GTL} />
-          Space Blaster
-        </a>
-      </div>
-      <br />
-      <img src={starhunter} alt="asteroid-gameplay" style={gifStyle} />
-      <h5>
-        Collect stars with your friends, be the first team to hit 200 to win!
-      </h5>
-      <div className="d-flex justify-content-center">
-        <a className="btn btn-outline-dark" href={`/gametwo?${room}`} role="button">
-          <img width="20px" style={styles} alt="starhunter" src={GTL} />
-          Star Hunter
-        </a>
-      </div>
-      <div>
-        <Form>
-          <Form.Group controlId="setUpRoom">
-            <Form.Label>Room Name</Form.Label>
-            <Form.Control type="text" placeholder="Enter Room Name" onChange={handleRoom} />
-          </Form.Group>
-          <Button variant="primary" type="button" onClick={roomToast}>
-            Submit
-          </Button>
-          <ToastContainer />
-        </Form>
-      </div>
-      <br />
-      <img src={starsollector} alt="starcollector-gameplay" style={gifStyle} />
-      <h5>
-        Collect stars from all platforms while avoiding bombs!
-      </h5>
-      <div className="d-flex justify-content-center">
-        <a className="btn btn-outline-dark" href="/gamethree" role="button">
-          <img width="20px" style={styles} alt="pick up stars" src={GTL} />
-          Pickup Stars
-        </a>
-      </div>
-      <br />
-      <img src={breakout} alt="breakout-gameplay" style={gifStyle} />
-      <h5>
-        Destroy all the blocks!
-      </h5>
-      <div className="d-flex justify-content-center">
-        <a className="btn btn-outline-dark" href="/breakout" role="button">
-          <img width="20px" style={styles} alt="breakout" src={GTL} />
-          Break Out
-        </a>
-      </div>
-      <br />
-      <img src={slime} alt="germs gif" style={gifStyle} />
-      <h5>
-        Avoid the germs!
-      </h5>
-      <div className="d-flex justify-content-center">
-        <a className="btn btn-outline-dark" href="/germs" role="button">
-          <img width="20px" style={styles} alt="germs" src={GTL} />
-          Germs!
-        </a>
-      </div>
-    </div>
-  );
-};
+        <MDBCarouselItem itemId="5">
+          <MDBView>
+            <img src={close} alt="Fifth slide" className="d-block w-100" />
+          </MDBView>
+          <MDBCarouselCaption>
+            <h5 href="/germs">
+              Germs!
+            </h5>
+          </MDBCarouselCaption>
+        </MDBCarouselItem>
+      </MDBCarouselInner>
+    </MDBCarousel>
+  </MDBContainer>
+);
 
 export default GamePage;
